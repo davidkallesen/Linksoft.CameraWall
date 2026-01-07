480272052c49782126da8764017c2d5565ba8434
davidkallesen <david@lmdk.dk>
2026-01-07 12:36:00 +0100

    feat: init solution setup

---
+++ atc-coding-rules-updater.ps1
@@ -0,0 +1,13 @@
+Clear-Host
+Write-Host "Update atc-coding-rules"
+dotnet tool update -g atc-coding-rules-updater
+
+$currentPath = Get-Location
+
+atc-coding-rules-updater `
+    run `
+    -p $currentPath `
+    --optionsPath $currentPath'\atc-coding-rules-updater.json' `
+    --buildFile MTS.Linksoft.Projects.sln `
+    --useTemporarySuppressions `
+    --verbose
